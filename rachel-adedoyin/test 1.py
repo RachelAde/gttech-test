bf410006a5c39bbe0255025243c1c03fb34bc21c
Rachel Adedoyin <43439558+RachelAde@users.noreply.github.com>
2018-11-29 10:19:17 +0100

    rachel-adedoyin test

---
+++ rachel-adedoyin/test 1.py
@@ -0,0 +1,4 @@
+def repeatStr(word):
+    newStr = word * 3
+    return newStr
+print(repeatStr(input("enter a word:")))
---
+++ rachel-adedoyin/test 3.py
@@ -0,0 +1,4 @@
+def nameChanger(name):
+    new_name = name[0] + name[-1]
+    return new_name
+print(nameChanger(input("enter your name:")))
